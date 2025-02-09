f5921619e1496b2c2dc49aaa203d9ce14c4d6dd1
Mydul Islam <si240147@gmail.com>
2025-02-09 23:50:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// let country = "bangladesh";
+// let divition = `Newkhele`;
+// let thana = new String("Pirgachha");
+// console.log(thana);
+// let upozila = "pirg accha";
+// console.log(upozila.length);
+// console.log(upozila[2]);
+// upozila[2] = "M"; //not work this code becouse string value not change its immutable
+// console.log(upozila);
+// let number = [56, 34, 23, 12, 23];
+// console.log(number.length);
+// console.log(number[2]);
+// number[1] = 12;
+// console.log(number);
+
+// lowercase and uppar case
+let school = "Mydul School and Collage";
+console.log(school.toUpperCase());
+
+let subject = "physics";
+let book = " Physics";
+if (subject.toLowerCase().trim() === book.toLowerCase().trim()) {
+  console.log("khali khob hbe");
+} else {
+  console.log("khali muri khai");
+}
