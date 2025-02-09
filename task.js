cabbe311aff097f31f21b87e759f6838af2857c4
Mydul Islam <si240147@gmail.com>
2025-02-09 23:59:18 +0600

    add

---
+++ task.js
@@ -0,0 +1,8 @@
+// task-1
+let country = "Bangladesh";
+console.log(country.indexOf("a"));
+console.log(country.length);
+console.log(country[3]);
+// task-2
+let bangladesh = "a";
+console.log(bangladesh.toUpperCase());
