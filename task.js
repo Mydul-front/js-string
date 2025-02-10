3286eaebb952b674c11fdd1852b547959dabfe6a
Mydul Islam <si240147@gmail.com>
2025-02-10 23:28:28 +0600

    add

--- task.js
+++ task.js
@@ -1,19 +1,45 @@
-// task-1
-let country = "Bangladesh";
-console.log(country.indexOf("a"));
-console.log(country.length);
-console.log(country[3]);
-// task-2
-let bangladesh = "a";
-console.log(bangladesh.toUpperCase());
-// vowel ber korar program
-let vowel = ["a", "e", "i", "o", "u"];
-// console.log(vowel.includes("i"));
-if (vowel.includes("i")) {
-  console.log("this is vowel");
-} else {
-  console.log("This is constant");
-}
+// // task-1
+// let country = "Bangladesh";
+// console.log(country.indexOf("a"));
+// console.log(country.length);
+// console.log(country[3]);
+// // task-2
+// let bangladesh = "a";
+// console.log(bangladesh.toUpperCase());
+// // vowel ber korar program
+// let vowel = ["a", "e", "i", "o", "u"];
+// // console.log(vowel.includes("i"));
+// if (vowel.includes("i")) {
+//   console.log("this is vowel");
+// } else {
+//   console.log("This is constant");
+// }
 // task-5
-let city = "bangladesh";
-console.log(city.charAt(0).toUpperCase());
+// let city = "bangladesh is a country of a big";
+// console.log(city.split(" "));
+// for(city.split(" ").toUpperCase()){
+//   console.log(city)
+// }
+
+// // task-4
+// let country = "bypzx";
+// let count = country.replace("y", "x");
+// console.log(count);
+
+// break
+
+// for (let i = 0; i < 150; i++) {
+//   console.log(i);
+//   if (i >= 5) {
+//     break;
+//   }
+// }
+
+let sentence = "hello world from javascript";
+
+sentence = sentence
+  .split(" ") // Split into words
+  .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()) // Capitalize the first letter of each word
+  .join(" "); // Join back into a string
+
+console.log(sentence); // "Hello World From Javascript"
