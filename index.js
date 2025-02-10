d4ae7b2cc52c4b56210bb66116a244d449325733
Mydul Islam <si240147@gmail.com>
2025-02-10 15:22:06 +0600

    add

--- index.js
+++ index.js
@@ -14,13 +14,42 @@
 // console.log(number);
 
 // lowercase and uppar case
-let school = "Mydul School and Collage";
-console.log(school.toUpperCase());
+// let school = "Mydul School and Collage";
+// console.log(school.toUpperCase());
 
-let subject = "physics";
-let book = " Physics";
-if (subject.toLowerCase().trim() === book.toLowerCase().trim()) {
-  console.log("khali khob hbe");
-} else {
-  console.log("khali muri khai");
+// let subject = "physics";
+// let book = " Physics";
+// if (subject.toLowerCase().trim() === book.toLowerCase().trim()) {
+//   console.log("khali khob hbe");
+// } else {
+//   console.log("khali muri khai");
+// }
+// // slice use to the problem
+// let address = "Rangpur";
+// let homeTown = address.slice(0, 3);
+// console.log(homeTown);
+// // split er maddhome vag kora hoy
+// let sentence = "I am a Good boy";
+// let boy = sentence.split(" ");
+// console.log(boy);
+
+// let name = "miran, mydul, mim, moon";
+// let friend = name.split(" ");
+// console.log(friend.join("|"));
+// console.log(friend.join("-"));
+// console.log(friend.concat(" ").concat(boy));
+
+// // simple vabe o concat korte pari
+// let firstName = "Mydul";
+// let lastName = "Islam";
+// fullName = firstName + " " + lastName;
+// console.log(fullName);
+// console.log(fullName.includes("M"));
+// reverse in the sentence mane ulta dik a suru hbe
+let sentence = "I am a Learning web devloper";
+let reverse = " ";
+for (let letter of sentence) {
+  // console.log(letter);
+  reverse = letter + reverse;
 }
+console.log(reverse);
--- task.js
+++ task.js
@@ -6,3 +6,14 @@ console.log(country[3]);
 // task-2
 let bangladesh = "a";
 console.log(bangladesh.toUpperCase());
+// vowel ber korar program
+let vowel = ["a", "e", "i", "o", "u"];
+// console.log(vowel.includes("i"));
+if (vowel.includes("i")) {
+  console.log("this is vowel");
+} else {
+  console.log("This is constant");
+}
+// task-5
+let city = "bangladesh";
+console.log(city.charAt(0).toUpperCase());
