9bbeb050ba46dcc063b5519b74adce06cf650c02
Mydul Islam <si240147@gmail.com>
2025-02-12 17:53:05 +0600

    add

--- task.js
+++ task.js
@@ -35,11 +35,78 @@
 //   }
 // }
 
-let sentence = "hello world from javascript";
+/* let sentence = "hello world from javascript";
 
 sentence = sentence
   .split(" ") // Split into words
   .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()) // Capitalize the first letter of each word
   .join(" "); // Join back into a string
 
-console.log(sentence); // "Hello World From Javascript"
+console.log(sentence); */ // "Hello World From Javascript"
+/* //charAt bebohar
+const names = "Bangladesh";
+console.log(names.charAt(3)); */
+//charCodeAt bebohar
+// const names = "Bangladesh";
+// console.log(names.charCodeAt(1));
+
+// concat er bebohar
+
+/* const names = "Bangladesh";
+const names1 = "Is a big Country";
+// console.log(names.concat(names1));
+let name = names.concat(" ").concat(names1);
+console.log(name);
+ */
+
+// lastIndexOf use to last index count in the value
+
+/* const names = "Banglhadesh";
+console.log(names.lastIndexOf("h"));
+console.log(names.indexOf("h")); */
+
+// const contry = "Bangladesh";
+// console.log(contry.indexOf("a"));
+
+// match(regex);
+// console.log("Hello 123".match(/\d+/));
+
+// replace value korar niyom
+
+// const names = "Bangladesh";
+// console.log(names.replace("desh", "gash"));
+
+// const names = "Bangladesh";
+// console.log(names.search("s"));
+
+// startWith use to string
+// const names = "This is bangladesh";
+// console.log(names.startsWith("miran"));
+
+// substring use to string mane slice
+
+/* const names = "Bangladesh";
+console.log(names.substring(0, 6)); */
+
+// const names = "    Bangladesh   ";
+// console.log(names.trim());
+
+// repeat use to string
+
+/* const names = "Bangladesh ";
+console.log(names.repeat(2)); */
+
+// padstart in the string
+// const values = "4";
+// console.log(values.padStart(4, "0"));
+
+// padEnd in the string
+
+// const number = "45";
+// console.log(number.padEnd(5, "4"));
+
+// Compare to the 2 string
+
+// const firstString = "country";
+// const secString = "country";
+// console.log(firstString.localeCompare(secString));
